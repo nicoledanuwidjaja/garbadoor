ab27cc77bcc360c3518fd1f9b4f8d52d5c45858d
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-10-26 09:35:27 -0400

    Dashboard updates and connectivity

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
-const Dashboard = () => (
-    <h1>Hi!</h1>
-);
+// Data from JSON objects (Firebase)
+import items from './items.json';
+
+// KendoReact UI
+import {Menu, PanelBar, TabStrip} from '@progress/kendo-react-layout';
+import {process} from '@progress/kendo-data-query';
+import {Grid, GridColumn} from '@progress/kendo-react-grid';
+import {DropDownList} from '@progress/kendo-react-dropdowns';
+import {Window} from '@progress/kendo-react-dialogs';
+import '@progress/kendo-theme-default/dist/all.css';
+
+class Dashboard extends React.Component {
+    state = {
+        dropdownlistCategory: null
+    }
+
+    handleDropDownChange = (e) => {
+        this.setState({
+            dropdownlistCategory: e.target.value.itemID
+        });
+    }
+
+    render() {
+        return (
+            <p>
+                <DropDownList
+                    data={items}
+                    dataItemKey="itemID"
+                    textField="categoryType"
+                    defaultItem={{itemID: null, categoryType: "Category type"}}
+                    onChange={this.handleDropDownChange}
+                />
+            </p>);
+    }
+}
 
 export default Dashboard;
