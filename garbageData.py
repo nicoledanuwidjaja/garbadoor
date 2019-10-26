7114970a0efd1f4b1988a3c94601117891558975
VKong6019 <vkong6019@gmail.com>
2019-10-26 01:48:28 -0400

    creds

--- garbageData.py
+++ garbageData.py
@@ -2,3 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+cred = credentials.Certificate('garbadoor-88843b8c25cf.json')
+firebase_admin.intialize_app(cred)
+
+db = firestore.client()
