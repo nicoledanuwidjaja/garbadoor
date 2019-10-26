528404caa21b6d250f08ff6a17b8ea94acb3a490
VKong6019 <vkong6019@gmail.com>
2019-10-26 03:07:25 -0400

    flask and firestore

--- garbageFlask.py
+++ garbageFlask.py
@@ -1,10 +1,15 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
+# TODO: two routes
+# - dashboard route
+# - camera route
+
 @app.route("/")
 def test():
-    return "placeholder"
+    return "I AM A garbAge"
 
 
 if __name__ == "__main__":
